f232c38b306a3672d5388832775d51ea8bfd16b0
Joey Nguyen <ngdinhhoang1205@gmail.com>
2025-06-02 14:47:30 +0700

    modified

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ import json
 import requests
 import time
 from BinanceStreamHandler import BinanceStreamHandler
-###############################################################################################################################################
+
 # Base stream
 base_url = 'wss://fstream.binance.com'
+###############################################################################################################################################
+# All Symbols
+all_symbol_url = "https://fapi.binance.com/fapi/v1/exchangeInfo"
+all_symbol_url_response = requests.get(all_symbol_url)
+all_symbols = all_symbol_url_response.json()
+all_symbols = [item['symbol'] for item in all_symbols['symbols']]
+###############################################################################################################################################
+
 # Combined streams
 combined_streams = 'wss://fstream.binance.com/stream' # output {"stream":"<streamName>","data":<rawPayload>}
 # Aggregate Trade Streams: The Aggregate Trade Streams push market trade information that is aggregated for fills with same price and taking side every 100 milliseconds. Only market trades will be aggregated, which means the insurance fund trades and ADL trades won't be aggregated.
@@ -23,7 +31,6 @@ agg_trade_keys_map = {
   "T": 'Trade time',
   "m": 'Is the buyer the market maker?',
 }
-###############################################################################################################################################
 # agg_trade_str processing
 stream_list = ["btcusdt@aggTrade", "ethusdt@aggTrade"]
 handler = BinanceStreamHandler(streams=stream_list, key_map=agg_trade_keys_map)
@@ -36,3 +43,4 @@ ws = websocket.WebSocketApp(
     on_open=handler.on_open
 )
 ws.run_forever()
+###############################################################################################################################################
